209eda34ccbfc793700f6c3d9dc6cd1664c73924
Joshua Hawking <joshuahawking1@gmail.com>
2021-05-20 17:33:22 +0100

    Allow clicking on challenge names to view challenge.

--- frontend/src/components/LockedChallenge.vue
+++ frontend/src/components/LockedChallenge.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="text-muted" v-if="challenge.locked">
-    <strong>Requires: </strong><a :href="`#challenge-${challenge.unlock_requirement}`">{{ requirementChallenge?.title }}</a>
+    <strong>Requires: </strong
+    ><a :href="`#challenge-${challenge.unlock_requirement}`" @click="openRequirementChallenge(challenge)">{{
+      requirementChallenge?.title
+    }}</a>
   </div>
   <hr />
   <div class="text-muted"><strong>Challenge Author: </strong>{{ challenge.author }}</div>
@@ -10,6 +13,14 @@
 import { defineProps } from "vue";
 
 import type { LockedChallenge, Challenge } from "../types/Challenge";
+
+const openRequirementChallenge = ({ unlock_requirement }: Challenge) => {
+  const accordion = document.getElementById(`challenge-${unlock_requirement}`);
+  if (!accordion) return;
+
+  const button = accordion.children[0] as HTMLElement | null;
+  if (button?.classList.contains("collapsed")) button.click();
+};
 
 defineProps({
   challenge: {
