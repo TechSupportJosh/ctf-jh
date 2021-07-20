ae046bfbf66797b40c2a0308828c724fbfdcb359
Joshua Hawking <joshuahawking1@gmail.com>
2021-07-20 20:18:52 +0100

    Added BS theme + particles for login.

--- frontend/src/routes/Login.vue
+++ frontend/src/routes/Login.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="vh-100 d-flex justify-content-center align-items-center text-center">
-    <div class="login-container">
-      <h1>Intake CTF</h1>
-      <h4 class="mb-4">Hosted by WMG Cyber Society</h4>
-      <div class="alert alert-danger" v-if="errorMessage"><strong>An error occured: </strong>{{ errorMessage }}</div>
-      <a :href="`${config.basePath}api/auth`" class="btn login-button text-white px-4">Login With Warwick ID</a>
+  <div class="vh-100">
+    <div id="main-container" class="vh-100">
+      <div id="login-container">
+        <h1>Intake CTF</h1>
+        <h4 class="mb-4">Hosted by WMG Cyber Society</h4>
+        <div class="alert alert-danger" v-if="errorMessage"><strong>An error occured: </strong>{{ errorMessage }}</div>
+        <a :href="`${config.basePath}api/auth/login`" class="btn text-white px-4" id="login-button">Login With Warwick ID</a>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
 import config from "../config";
+import { tsParticles } from "tsparticles";
+import particlesConfig from "../assets/particles.json";
 
 const route = useRoute();
 const errorCode = route.query.error;
@@ -26,15 +30,34 @@ switch (errorCode) {
     errorMessage.value = "You do not have access to this site, please contact an administrator";
     break;
 }
+
+onMounted(() => {
+  tsParticles.load("main-container", particlesConfig as any);
+});
 </script>
 
-<style scoped>
-.login-button {
+<style scoped lang="scss">
+#login-button {
   background-color: #5b3069;
 }
-.login-container {
-  background-color: white;
-  padding: 50px;
+#main-container {
+  display: flex;
+  vertical-align: bottom;
+  min-height: 100vh;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-attachment: fixed;
+}
+#login-container {
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin: 0 auto;
+  -webkit-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  padding: 40px;
   border-radius: 10px;
+  background-color: var(--bs-dark);
 }
 </style>
