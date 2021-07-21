48c7794da64bad72ddc9b620c72c1f6c15a1c969
Joshua Hawking <joshuahawking1@gmail.com>
2021-07-21 15:52:10 +0100

    Remove debug challenges.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -44,51 +44,6 @@ app.use("/api", router);
     entities: ["src/entity/*.ts"],
   });
 
-  // const demoChallenge = new Challenge();
-  // demoChallenge.disabled = false;
-  // demoChallenge.author = "Josh";
-  // demoChallenge.description = "It's a demo challenge";
-  // demoChallenge.difficulty = "Easy";
-  // demoChallenge.flag = "WMG{Test Flag}";
-  // demoChallenge.hint = "This is a hint";
-  // demoChallenge.points = 30;
-  // demoChallenge.category = "Web";
-  // demoChallenge.title = "Demo Challange";
-  // demoChallenge.educationResources = [];
-  // demoChallenge.tags = [];
-
-  // await demoChallenge.save();
-
-  // const tag = new ChallengeTag();
-  // tag.tag = "LFI";
-  // tag.challenge = demoChallenge;
-  // await tag.save();
-
-  // const tag2 = new ChallengeTag();
-  // tag2.tag = "RFI";
-  // tag2.challenge = demoChallenge;
-  // await tag2.save();
-
-  // const resource = new EducationResource();
-  // resource.resource = "https://google.com";
-  // resource.challenge = demoChallenge;
-  // await resource.save();
-
-  // const lockedChallenge = new Challenge();
-  // lockedChallenge.disabled = false;
-  // lockedChallenge.author = "Josh";
-  // lockedChallenge.description = "It's the sequel!";
-  // lockedChallenge.difficulty = "Hard";
-  // lockedChallenge.flag = "WMG{Test Flag 2}";
-  // lockedChallenge.hint = "This is a hint";
-  // lockedChallenge.points = 70;
-  // lockedChallenge.category = "Web";
-  // lockedChallenge.title = "Demo Locked Challenge";
-  // lockedChallenge.educationResources = [];
-  // lockedChallenge.tags = [];
-  // lockedChallenge.unlockRequirement = demoChallenge;
-  // await lockedChallenge.save();
-
   app.listen(8080, () => {
     console.log("Listening on port 8080...");
   });
