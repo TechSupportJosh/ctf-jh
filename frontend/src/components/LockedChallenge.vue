824fbf7f8d0c999ad0ef46207bf5d23f191ebc5f
Joshua Hawking <joshuahawking1@gmail.com>
2021-07-21 11:52:51 +0100

    Remove hyperlinks to locked challenges.

--- frontend/src/components/LockedChallenge.vue
+++ frontend/src/components/LockedChallenge.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="text-muted" v-if="challenge.locked">
-    <strong>Requires: </strong
-    ><a :href="`#challenge-${challenge.unlockRequirement}`" @click="openRequirementChallenge(challenge)">{{
-      requirementChallenge?.title
-    }}</a>
+    <strong>Requires: </strong><span class="text-white">{{ requirementChallenge?.title }}</span>
   </div>
   <hr />
   <div class="text-muted"><strong>Challenge Author: </strong>{{ challenge.author }}</div>
@@ -13,14 +10,6 @@
 import { defineProps } from "vue";
 
 import type { LockedChallenge, Challenge } from "../types/Challenge";
-
-const openRequirementChallenge = ({ unlockRequirement }: Challenge) => {
-  const accordion = document.getElementById(`challenge-${unlockRequirement}`);
-  if (!accordion) return;
-
-  const button = accordion.children[0] as HTMLElement | null;
-  if (button?.classList.contains("collapsed")) button.click();
-};
 
 defineProps({
   challenge: {
