51404116a5ed342b6526f40c43ad7b7c550003de
Joshua Hawking <joshuahawking1@gmail.com>
2021-09-28 22:20:10 +0100

    Remove unneeded middlewares on self.

--- backend/src/routes/self.ts
+++ backend/src/routes/self.ts
@@ -6,16 +6,16 @@ import { EventType, logEvent } from "../utils/log";
 
 const router = express.Router();
 
-router.get("/", isAuthenticated(), (req, res) => {
+router.get("/", (req, res) => {
   res.json(req.user!.toSelfJSON());
 });
 
-router.get("/stats", isAuthenticated(), async (req, res) => {
+router.get("/stats", async (req, res) => {
   const user = await User.findOne({ where: { id: req.user!.id }, relations: ["stats", "solveAttempts"] });
   res.json(user!.stats);
 });
 
-router.get("/sessions", isAuthenticated(), async (req, res) => {
+router.get("/sessions", async (req, res) => {
   const sessions = await UserAuth.createQueryBuilder("userAuth")
     .select("userAuth.creationDate")
     .addSelect("userAuth.ipAddress")
@@ -26,7 +26,7 @@ router.get("/sessions", isAuthenticated(), async (req, res) => {
   res.json(sessions);
 });
 
-router.delete("/sessions", isAuthenticated(), async (req, res) => {
+router.delete("/sessions", async (req, res) => {
   const sessions = await UserAuth.find({ where: { user: req.user } });
 
   await Promise.all(sessions.map((session) => session.remove()));
@@ -36,7 +36,7 @@ router.delete("/sessions", isAuthenticated(), async (req, res) => {
   return res.status(200).send({ message: "Sessions deleted." });
 });
 
-router.delete("/sessions/:authId", isAuthenticated(), async (req, res) => {
+router.delete("/sessions/:authId", async (req, res) => {
   const userAuth = await UserAuth.findOne({ where: { user: req.user, authId: req.params.authId } });
 
   if (!userAuth) return res.status(404).send({ message: "This session does not exist." });
